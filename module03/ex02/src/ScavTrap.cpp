e222143dbfcbe6b4b3a66b9bc11476f6827b61b0
Mitani Misumi <mmisumi@f0r2s15.codam.nl>
2026-03-16 15:08:31 +0100

    pushed altered scavtrap in ex02

--- module03/ex02/src/ScavTrap.cpp
+++ module03/ex02/src/ScavTrap.cpp
@@ -8,7 +8,6 @@ ScavTrap::ScavTrap() :  ClapTrap()
 ScavTrap::ScavTrap(const std::string& name) : ClapTrap(name)
 {
 	std::cout << "ScavTrap " << _name << " constructor called\n";
-
 	_hitPoints = 100;
 	_energyPoints = 50;
 	_attackDamage = 20;
@@ -27,14 +26,36 @@ ScavTrap::ScavTrap(const ScavTrap& other) : ClapTrap(other)
 ScavTrap&	ScavTrap::operator=(const ScavTrap& other)
 {
 	std::cout << "ScavTrap " << _name << " copy assignment operator called\n";
-
 	if (this != &other) {
 		ClapTrap::operator=(other);
 	}
 	return (*this);
 }
 
+void	ScavTrap::attack(const std::string &target)
+{
+	if (_hitPoints < 1) {
+		std::cout << _name << " is dead and cannot attack.\n";
+		return ;
+	}
+	if (_energyPoints < 1) {
+		std::cout << _name << " has no energypoints left to attack.\n";
+		return ;
+	}
+	std::cout << "ScavTrap " << _name << " attacks " << target
+			  << ", causing " << _attackDamage << " points of damage!\n";
+	_energyPoints -= 1;
+}
+
 void	ScavTrap::guardGate()
 {
+	if (_hitPoints < 1) {
+		std::cout << _name << " is dead and cannot guard the gate.\n";
+		return ;
+	}
+	if (_energyPoints < 1) {
+		std::cout << _name << " has no energypoints left to guard the gate.\n";
+		return ;
+	}
 	std::cout << _name << " is now in Gate keeping mode.\n";
 }
